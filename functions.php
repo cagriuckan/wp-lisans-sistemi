d950f561cbc380f40bf1204ea51144568795c22b
Çağrı Üçkan <uckancagri@gmail.com>
2021-03-06 12:24:18 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,5 @@
 <?php
-require(get_template_directory() . '/license.php');
-$licensing = new Kan_Licensing_System;
+require get_template_directory() . '/license.php';
+$licensing = new Kan_Licensing_System(
+	'http://localhost/wordpress/server.json'
+);
